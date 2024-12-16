915802605229f8aaea22b998d0b6527eed1c9e1b
nischal puri <mrnischalpuri@gmail.com>
2024-12-16 19:25:47 +0545

    tailwindcss theme setup with custom screens, container, and fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "340px",
+      md: "540px",
+      lg: "768px",
+      xl: "1180px",
+    },
+    fontFamily: {
+      jost: ["Jost", "sans-serif"],
+      lobster: ["Lobster", "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "12px",
+        md: "32px",
+      },
+    },
     extend: {},
   },
   plugins: [],
